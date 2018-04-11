18ee76c62a34b93c8d564c25957a59056d791c0c
Riya Varughese <rav294@nyu.edu>
2018-04-10 20:52:07 -0400

    Add files via upload

---
+++ micReading.ino
@@ -0,0 +1,47 @@
+#include "arduinoFFT.h"
+#define SAMPLES 128
+#define SAMPLING_FREQUENCY 1000
+
+arduinoFFT FFT = arduinoFFT();
+unsigned int sampling_period_us;
+unsigned long microseconds;
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for(int i=0; i<SAMPLES; i++)
+  {
+    microseconds = micros();
+    vReal[i] = analogRead(A0);
+    vImag[i] = 0;
+    while(micros()<(microseconds + sampling_period_us)){}
+    //Serial.println(vReal[i]);
+    }
+    //Serial.println(vReal[1]);
+
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY); 
+
+    //Serial.print("peak = ");
+
+    //Serial.println(peak);
+    
+  for (int i=0; i<(SAMPLES/2); i++){
+    Serial.println(vReal[i],1);
+    }
+    //delay(1000);
+    while(1);
+   // Serial.println("end");
+//int sensorVal = analogRead(A0);
+
+}
---
+++ piazzo_buzzer.ino
@@ -0,0 +1,43 @@
+
+void setup() {
+  // put your setup code here, to run once:
+//pinMode(10, OUTPUT);
+Serial.begin(9600);
+
+//ADC0 Configuration register 1
+//ADC0_CFG1 |= _BV(5) | (1U << 6); // clock division of 8. 9MHZ, 
+//ADC0_CFG1 |= (1U << 2) | (1U << 3); //16 bit conversion mode
+//ADC0_CFG1 = 0b00000000000000000000000000001100;
+
+//ADC0 configuration register 2
+//ADC0_CFG2 |= _BV(4); //This selects ADXXb, in our case AD05b or just AD5b
+//ADC0_CFG2 |= ADC_CFG2_MUXSEL; //This does the same thing as above
+//ADC0_CFG2 = 0b00000000000000000000000000010000;
+
+//ADC0 Programmable gain amplifier
+//ADC0_PGA |= ADC_PGA_PGAEN; //Use this line if you want to use programmable gain amplified that Professor discussed
+
+//ADC0 Status and control register 3
+
+//ADC0_SC3 |= ADC_SC3_ADCO | ADC_SC3_AVGE; //These are in kinetis.h, enables continuous mode and averaging function. You don't NEED averaging function. 
+//ADC0_SC3 |= ADC_SC3_ADCO ;
+
+//ADC0 Status control register 1
+//ADC0_SC1A = 5; // or binary 0101, it is equal not OR equal since I disabled the other configurations on purpose.
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//tone(10,2000);
+//delay(1000);
+//noTone (10);
+//delay (1000);
+//uint16_t data = ADC0_RA; // Data result register A
+
+int sensorVal = analogRead(A0);
+
+Serial.println(sensorVal);
+delay(10);
+}
+
